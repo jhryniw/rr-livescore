842e93db967e486b901ed5f727234e0d6b350c44
James Hryniw <jhryniw@gmail.com>
2017-12-23 21:24:29 -0700

    Paging bottom navigation drawer (#5)
    * add firebase datasource
    
    * autonomous and teleop tabbing
    
    * navigation paging

--- app/src/main/java/ca/ftcalberta/rrlivescore/ScoringActivity.java
+++ app/src/main/java/ca/ftcalberta/rrlivescore/ScoringActivity.java
@@ -3,42 +3,79 @@ package ca.ftcalberta.rrlivescore;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.ViewGroup;
 
-public class ScoringActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private TextView mTextMessage;
+public class ScoringActivity extends FragmentActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener,
+        ViewPager.OnPageChangeListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @BindView(R.id.fragment_container)
+    ViewPager viewPager;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AutonomousFragment(), "Autonomous");
+        adapter.addFragment(new TeleopFragment(), "Teleop");
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(this);
+
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.navigation_autonomous:
+                if (viewPager.getCurrentItem() != 0) {
+                    viewPager.setCurrentItem(0);
+                }
+                return true;
+            case R.id.navigation_teleop:
+                if (viewPager.getCurrentItem() != 1) {
+                    viewPager.setCurrentItem(1);
+                }
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+            case 0:
+                navigation.setSelectedItemId(R.id.navigation_autonomous);
+                break;
+            case 1:
+                navigation.setSelectedItemId(R.id.navigation_teleop);
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
---
+++ app/src/main/java/ca/ftcalberta/rrlivescore/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package ca.ftcalberta.rrlivescore;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Pager adapter to handle fragment tabs
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    /**
+     * Add a new fragment to the pager
+     * @param fragment the new fragment
+     * @param title the title of the fragment/tab
+     */
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
